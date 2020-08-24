417514ca5da55058bfa4777f4533fd7dbc94524f
Zeeshaan Maudarbocus <zeeshaan@touchfoundry.co.za>
2020-08-24 22:10:03 +0200

    add some front end validation

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -3,38 +3,67 @@ import React, { useState } from 'react'
 const Create = () => {
     const [description, setDescription] = useState('');
     const [file, setFile] = useState(null);
+    const [error, setError] = useState('');
 
     console.log('file: ', file);
 
     const handleSubmit = async event => {
         event.preventDefault();
 
-        const formData = new FormData();
+        if (!description) {
+            setError('Please add a description');
+            return;
+        }
 
+        if (!file) {
+            setError('Please add a file');
+            return;
+        }
+
+        const formData = new FormData();
         formData.append('data', JSON.stringify({ description }));
         formData.append('files.image', file);
 
-        const response = await fetch('http://localhost:1337/posts', {
-            method: 'POST',
-            body: formData
-        });
+        try {
 
-        const data = await response.json();
+            const response = await fetch('http://localhost:1337/posts', {
+                method: 'POST',
+                body: formData
+            });
+
+            const data = await response.json();
+
+            console.log('data: ', data);
+
+        } catch(err) {
+            console.log('Exception: ', err);
+            setError(err);
+        }
+
     };
 
     return (
         <div className='Craete'>
             <h2>Create</h2>
+
+            {error && <p>{error}</p>}
+
             <form onSubmit={handleSubmit}>
                 <input
                     placeholder='Description'
                     value={description}
-                    onChange={event => setDescription(event.target.value)}
+                    onChange={event => {
+                        setError('')
+                        setDescription(event.target.value)
+                    }}
                 />
                 <input
                     type='file'
                     placeholder='Add a file'
-                    onChange={event => setFile(event.target.files[0])}
+                    onChange={event => {
+                        setError('')
+                        setFile(event.target.files[0])
+                    }}
                 />
                 <button>Submit</button>
             </form>
