8f751d4a5d3a6b3e710e145b1486d9c478f22152
Zeeshaan Maudarbocus <zeeshaan@touchfoundry.co.za>
2020-08-24 21:19:55 +0200

    import new components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import Nav from './components/Nav';
+
 import Home from './pages/Home';
+import Create from './pages/Create';
 
 import './App.css';
 
@@ -9,8 +12,10 @@ const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
+        <Nav />
         <Switch>
             <Route path='/' exact component={Home} />
+            <Route path='/create' exact component={Create} />
         </Switch>
       </BrowserRouter>
     </div>
