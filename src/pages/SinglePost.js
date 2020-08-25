735384ff2a326a86f8644cba58fb918cfe3f7146
Zeeshaan Maudarbocus <zeeshaan@touchfoundry.co.za>
2020-08-25 11:41:54 +0200

    add delete logic

--- src/pages/SinglePost.js
+++ src/pages/SinglePost.js
@@ -2,12 +2,20 @@ import React, { useState, useEffect } from 'react';
 
 import Post from '../components/Post';
 
-const SinglePost = ({ match }) => {
+const SinglePost = ({ match, history }) => {
 
     const [post, setPost] = useState({});
     const [loading, setLoading] = useState(true);
 
     const { id } = match.params;
+
+    const handleDelete = async () => {
+        const response = await fetch(`http://localhost:1337/posts/${id}`, {
+            method: 'DELETE'
+        });
+        const data = await response.json();
+        history.push('/');
+    };
 
     useEffect(() => {
         const fetchPost = async () => {
@@ -28,7 +36,10 @@ const SinglePost = ({ match }) => {
                 <p>Loading...</p>
             )}
             {!loading && (
-                <Post post={post} />
+                <>
+                    <Post post={post} />
+                    <button onClick={handleDelete}>Delete</button>
+                </>
             )}
         </div>
     )
