683b8f61d098393ab96e5b4345c3fe69f190cf82
Zeeshaan Maudarbocus <zeeshaan@touchfoundry.co.za>
2020-08-25 11:13:36 +0200

    fetch single post from strapi using useState for componentDidMount and useEffect to call async function.

--- src/pages/SinglePost.js
+++ src/pages/SinglePost.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const SinglePost = () => {
+import Post from '../components/Post';
+
+const SinglePost = ({ match }) => {
+    const [post, setPost] = useState({});
+
+    const { id } = match.params;
+
+    useEffect(() => {
+        const fetchPost = async () => {
+            const response = await fetch(`http://localhost:1337/posts/${id}`);
+            const data = await response.json();
+
+            console.log('data: ', data);
+            setPost(data);
+        }
+
+        fetchPost();
+    });
+
     return (
-        <div>
-            Single Post
+        <div className='SinglePost'>
+            <Post post={post} />
         </div>
     )
 }
